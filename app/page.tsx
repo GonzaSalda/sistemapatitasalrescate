de0858b315b3243d2e625c6f123e747a11eaa363
GonzaSalda <gonza.salda00@gmail.com>
2024-07-23 01:23:32 -0300

    Add PatientForm component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex gap-x-2 items-center mb-12">
+            <Image
+              src="/assets/icons/logo-icon.svg"
+              alt="logo"
+              width={1000}
+              height={1000}
+              className="h-10 w-fit"
+            />
+            <div className="h-full">
+              <h1 className="uppercase">Patitas al rescate</h1>
+            </div>
+          </div>
 
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">@ 2024 PatitasAlRescate</p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="onboarding"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
